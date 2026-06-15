76d8243e82a56bd2fe6b3ed0405676d1d6185c24
Abhishek-K-Vishvakarma <gitrepo101@gmail.com>
2026-06-16 01:25:34 +0530

    fix: create uploads directory before file upload

--- ai-resume-analyzer-server/src/middleware/upload.middleware.js
+++ ai-resume-analyzer-server/src/middleware/upload.middleware.js
@@ -1,19 +1,24 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
+
+const uploadDir = "uploads";
+
+// Create uploads folder if it doesn't exist
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, uploadDir);
   },
 
   filename: (req, file, cb) => {
     const uniqueName =
       Date.now() + "-" + Math.round(Math.random() * 1e9);
 
-    cb(
-      null,
-      uniqueName + path.extname(file.originalname)
-    );
+    cb(null, uniqueName + path.extname(file.originalname));
   },
 });
 
